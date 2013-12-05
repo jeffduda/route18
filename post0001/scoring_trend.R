bf437ed31af206e1ab73f9c1c8fc4ea774b32df0
Jeffrey Duda <jeff.duda@gmail.com>
2013-12-05 14:04:44 -0500

    ADD: final code for post

--- post0001/scoring_trend.R
+++ post0001/scoring_trend.R
@@ -2,22 +2,57 @@ require(ggplot2)
 require(colorspace)
 
 w <- read.csv("../data/2013_weekly.csv")
-totals <- data.frame(Points=w$QB+w$RB1+w$RB2+w$WR1+w$WR2+w$TE+w$D+w$K+w$FLEX, Week=w$week, Team=w$team, Win=w$result)
-g <- ggplot(totals, aes(x=Week, y=Points, group=Win, colour=Win)) + geom_point() + geom_smooth(method=lm,fullrange=TRUE) + scale_x_continuous(limits = c(2, 16)) + ggtitle( "Route 18's Finest - 2013 Scoring trend (by W/L)" )
+
+ranks <- c("john","frank","brian","nate","owen","pete","anthony","dave","ryan","steve","fred","jeff")
+w$team <- factor(w$team, levels=ranks)
+
+totals <- data.frame(Points=w$QB+w$RB1+w$RB2+w$WR1+w$WR2+w$TE+w$D+w$K+w$FLEX, Week=w$week, Team=w$team, Result=w$result)
+                  
+
+g <- ggplot(totals, aes(x=Week, y=Points, group=Result, colour=Result)) + geom_point() + geom_smooth(method=lm,fullrange=TRUE) + scale_x_continuous(limits = c(1, 16)) + ggtitle( "Route 18's Finest - 2013 Scoring trend (by W/L)" )
 png(filename="scoring_trend.png", width=600, height=300)
 print(g)
 dev.off()
 
-colours <- c("#2121D9", "#9999FF", "#D92121", "#21D921", "#FFFF4D", "#FF9326")
-ltype <- c(rep(1,6),rep(2,6))
 
-g <- ggplot(totals, aes(x=Week, y=Points, group=Team, colour=Team)) + geom_point() + geom_smooth(method=lm,fullrange=TRUE,se=FALSE) + scale_x_continuous(limits = c(2, 16)) + ggtitle( "Route 18's Finest - 2013 Scoring trend (by team)" ) + scale_fill_manual(values=colours)
+cbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#CC79A7")
+cbPalette <- c(cbPalette, cbPalette)
+
+g2 <- ggplot(totals, aes(x=Week, y=Points, group=Team, colour=Team, linetype=Team )) + geom_point() + scale_colour_manual(values=cbPalette) + geom_smooth(method="lm",fullrange=TRUE,se=FALSE,size=1) + scale_linetype_manual(values=c(rep("solid",6),rep("dashed",6)) )  + scale_x_continuous(limits = c(1, 16)) + ggtitle( "Route 18's Finest - 2013 Scoring trend (by team)" )
 png(filename="scoring_trend_team.png", width=600, height=300)
-print(g)
+print(g2)
 dev.off()
 
+print( "made figures" )
+#print(summary(lm(totals$Points ~ 1 + totals$Week, subset=which(totals$Win=="W"))))
+#print(summary(lm(totals$Points ~ 1 + totals$Week, subset=which(totals$Win=="L"))))
+
+xw <- summary(lm(totals$Points ~ 1 + totals$Week, subset=which(totals$Result=="W")))
+xl <- summary(lm(totals$Points ~ 1 + totals$Week, subset=which(totals$Result=="L")))
+
+pw <- pf(xw$fstatistic[1], xw$fstatistic[2], xw$fstatistic[3], lower.tail=FALSE)
+pl <- pf(xl$fstatistic[1], xl$fstatistic[2], xl$fstatistic[3], lower.tail=FALSE)
+
+print( paste("win",pw))
+print( paste("lose",pl))
+
+for( team in ranks ) {
+  tx <- which(totals$Team==team)
+  pts <- totals$Points[tx]
+  week <- totals$Week[tx]
+  
+  lx <- lm(pts ~ 1 + week)
+  x <- summary(lx)
+  pval <- pf(x$fstatistic[1], x$fstatistic[2], x$fstatistic[3], lower.tail=FALSE)
+  #print( paste(team, pval) )
+
+  ftr <- data.frame(week=c(14,15,16))
+  preds <- predict(lx,ftr, se.fit=TRUE)
+
+  print( paste( team, "14", preds$fit[1], "+/-", preds$se.fit[1] ) )
+  #print( paste( team, "15", preds$fit[2], "+/-", preds$se.fit[2] ) )
+  #print( paste( team, "16", preds$fit[3], "+/-", preds$se.fit[3] ) )
+
+}
 
 
-
-print(summary(lm(totals$Points ~ 1 + totals$Week, subset=which(totals$Win=="W"))))
-print(summary(lm(totals$Points ~ 1 + totals$Week, subset=which(totals$Win=="L"))))
